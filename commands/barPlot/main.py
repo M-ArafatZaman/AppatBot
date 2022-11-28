8b2c90c754560bbb087f11ace85903700af70b59
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-11-28 22:13:27 +0600

    Updated barplot function to use an API instead

--- commands/barPlot/main.py
+++ commands/barPlot/main.py
@@ -1,29 +1,43 @@
-import matplotlib.pyplot as plt
 from .isBarPlot import extractBarPlotParameters
 from typing import List
 import io
 import discord
+import requests
+from core.validation.error import error_message
 
-async def barplot(ctx, _cmd: str):
+async def barplot(ctx: discord.Message, _cmd: str):
     """
     This command sends an image of the barplot
     """
 
-    # Generate x and y
-    y: List[float] = extractBarPlotParameters(_cmd)
-    x: List[int] = [i+1 for i in range(len(y))]
+    ENDPOINT = "https://funapi.onrender.com/api/barplot/"
 
-    # Plot data
-    plt.bar(x, y, label="Numbers visualized")
-    plt.legend(loc="upper right")
-    fig = plt.gcf()
-
-    # Create input buffer
-    buf = io.BytesIO()
-    fig.savefig(buf, format="png")
-    buf.seek(0)
-    fig.clear()
-    plt.close()
+    # Generate y
     
-    await ctx.channel.send(file=discord.File(buf, "plot.png"))
-    buf.close()
+    # Try parsing the parameters
+    try:
+        y: List[float] = extractBarPlotParameters(_cmd)
+
+        # Parse is successful, send request
+        y_str: List[str] = [str(i) for i in y]
+        resp = requests.get(f'{ENDPOINT}?data={",".join(y_str)}')
+
+        if resp.status_code == 200 and resp.headers["Content-Type"] == "image/png":
+            # Got successful image return
+            buffer = io.BytesIO(resp.content)
+            image = discord.File(buffer, "plot.png")
+            
+            # Send image
+            await ctx.channel.send(file=image)
+
+            # Close buffer
+            buffer.close()
+
+    except Exception:
+        await error_message(
+            ctx,
+            title="Invalid usage", 
+            message="Please enter numeric parameters"
+        )
+        return
+
